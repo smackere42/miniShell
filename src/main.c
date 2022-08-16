bbdb26932da06b966fea9b57131a88a23c4b3710
Александр Кошелев <alekoshelev@ozon.ru>
2022-08-17 00:01:11 +0300

    working main

---
+++ src/main.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmumm <kmumm@student.21-school.ru>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/16 23:23:09 by kmumm             #+#    #+#             */
+/*   Updated: 2022/08/16 23:53:31 by kmumm            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+void	emulate_ctrl_c(int sig_num)
+{
+	(void) sig_num;
+	write(STDERR_FILENO, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		++s;
+		++len;
+	}
+	return (len);
+}
+
+int	check_exit_eof(char *read)
+{
+	if (!read)
+	{
+		write(2, "exit\n", 5);
+		return (1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*read;
+
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, emulate_ctrl_c);
+	while (1)
+	{
+		read = readline(PROMPT);
+		if (check_exit_eof(read))
+			break ;
+		if (ft_strlen(read) > 0)
+			add_history(read);
+		free(read);
+	}
+	return (0);
+}
